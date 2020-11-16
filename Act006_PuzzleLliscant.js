f4fce1fa0c3ed24b3ea8fa0b34059dd9707484f1
SergiSM7 <65180552+SergiSM7@users.noreply.github.com>
2020-11-16 19:53:03 +0100

    Activitat 06 PuzzleLliscant

---
+++ Act006_PuzzleLliscant.js
@@ -0,0 +1,18 @@
+let cos = document.getElementById("cos");
+let taula = document.getElementById("taula");
+
+let fila, cella;
+
+for (let i=1; i<3; i++) {
+    fila = taula.insertRow();
+    for (let j=1; j<3; j++){
+        cella = fila.insertCell(-1)
+        cella.id="c" + i + j;
+        cella.innerHTML=i+ ", "+j;
+    }
+}
+
+
+function canvia (celaFila, celaColumna) {
+    alert("Has pitjat sobre la cel·la " + celaFila + ", " + celaColumna);
+};
